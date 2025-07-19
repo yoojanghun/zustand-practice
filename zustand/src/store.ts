c91fc17ae5fc5f3061949bed9a1ebe1167825d97
yoojanghun <yoojanghun@naver.com>
2025-07-19 11:37:35 +0900

    상태 바꾸는 함수 작성법

--- zustand/src/store.ts
+++ zustand/src/store.ts
@@ -9,16 +9,16 @@ type CounterStore = {
 export const useCounterStore = create<CounterStore>((set) => ({
     count: 0,
     increment: () => {
-        set({count: 1});
+        set((state) => ({ count: state.count + 1 }));
     },
     decrement: () => {
-        set({count: -1});
+        set((state) => ({ count: state.count - 1 }));
     },
 }));
 
-// create : Zustand에서 전역으로 사용 가능한 상태 저장소 생성
-// 함수는 내부에 상태값, 상태를 바꾸는 함수를 객체로 받고 
-// useCounterStore라는 커스텀 훅을 반환
-// useCounterStore()를 React 컴포넌트에서 호출하면 그 전역 상태를 사용 가능
-
-// set은 create함수가 자동으로 제공해주는 상태 업데이트 함수
+// 위의 코드에서 state엔 현재 store의 상태 전체 객체가 들어간다.
+// state = {
+//      count: 현재값,
+//      increment: 함수,
+//      decrement: 함수,
+// }
